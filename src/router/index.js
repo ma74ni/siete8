ddde3fde6d81b0b755723163025d9406ba62b298
MA74NY <ma74ny@gmail.com>
2020-07-14 21:52:50 -0500

    Off Line Site

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,32 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
+const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Maintenance",
+    component: () =>
+      import(/* webpackChunkName: "maintenance" */ "../views/Maintenance.vue"),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/home",
+    name: "Home",
+    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue"),
+  },
+  {
+    path: "/brief",
+    name: "Brief",
+    component: () =>
+      import(/* webpackChunkName: "brief" */ "../views/Brief.vue"),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/Maintenance.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo_siete8.svg" />
+    <OffLine msg="Sitio en manteniento" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import OffLine from "@/components/OffLine.vue";
+
+export default {
+  name: "Maintenance",
+  components: {
+    OffLine,
+  },
+};
+</script>
